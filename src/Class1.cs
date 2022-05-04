438a39492739133e2c992fdbf859900102b0f3c7
Rennorb <lplucas99@gmail.com>
2022-05-05 00:32:00 +0200

    initial commit

---
+++ src/Class1.cs
@@ -0,0 +1,35 @@
+﻿using Rennorb.Logging;
+
+namespace Test {
+	public class Program {
+
+		Logger logger = new("Main");
+
+		bool shutdown_clean = false;
+
+		public Program(Action request_stop)
+		{
+
+		}
+
+		public int Main(string[] args)
+		{
+			Console.WriteLine("start");
+
+			Thread.Sleep(3000);
+
+			return shutdown_clean ? 0 : 1; //error
+		}
+
+		public void  ConsoleCommandReceived(string command)
+		{
+			shutdown_clean = true;
+		}
+
+		public void Stop()
+		{
+			logger.WriteLine("stop requested");
+		}
+
+	}
+}
